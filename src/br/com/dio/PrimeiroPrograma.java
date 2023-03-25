c2a0864906949904915e7d4c4ddaba5620f3b156
Marcelo Paiva - Windows <marcelojapaiva@gmail.com>
2023-03-25 11:08:44 -0300

    Apenas uma frase para o commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,10 +6,47 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
+        Livro livro1 = new Livro("os 3 porquinhos",150);
+
         System.out.println(gato);
+        System.out.println(livro1);
         /* int a = 5;
         int b = 3;
         System.out.println("Hello World!! "+ (a+b));*/
 
+    }
+}
+
+class Livro{
+    private String Nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        Nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "Nome='" + Nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
